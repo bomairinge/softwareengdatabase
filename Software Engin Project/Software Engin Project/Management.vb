f5a7fc83c4ed74a203c3beaefd495eb11b79dee6
SeanAkin <56386301+SeanAkin@users.noreply.github.com>
2019-10-22 00:53:33 +0100

    Management C#
    Management Page Added :)

--- Software Engin Project/Software Engin Project/Management.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Management
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-End Class
