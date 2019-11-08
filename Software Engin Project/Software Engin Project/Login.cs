0926829054682c3db8deda1486927f1bf702db47
SeanAkin <56386301+SeanAkin@users.noreply.github.com>
2019-11-08 23:06:21 +0000

    Login Page
    Login added with code. A message box is needed.
    
    A few touches may be necessary later.

---
+++ Software Engin Project/Software Engin Project/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Software_Engin_Project
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // DatabaseConnection.Sample.createDataSet test = ("Select Count(*) from Employee where Username'" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
+            // DataSet employeedata = new Sample.("Select Count(*) from Employee where Username'" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
+            int rows = DatabaseConnection.Sample.getSelectCount("Select Count(*) from Employee where Username ='" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
+            /*  DataSet data = DatabaseConnection.createDataSet("Select Count(*) from Employee where Username ='" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
+              DataTable dt = new DataTable();
+              dt = data.Tables[0];
+
+              //this.Hide(); // Code will need to go    nto an IF statement once database connection created
+              //BedOverview bed = new BedOverview(); // Code will need to go into an IF statement once database connection created
+              //bed.Show(); // Code will need to go into an IF statement once database connection created
+
+
+
+              if (dt.Rows[0][0].ToString() == "1")
+           */
+              if(rows==1)
+            {
+                this.Hide(); 
+                BedOverview bed = new BedOverview(); 
+                bed.Show(); 
+            }
+            else
+            {
+            //Messagebox.Show("No Username or Password found with those credentials");
+            } 
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            LoginAlarmPic1.Hide();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Software Engin Project/Software Engin Project/Set Alarms.Designer.cs
+++ Software Engin Project/Software Engin Project/Set Alarms.Designer.cs
@@ -37,7 +37,6 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -141,15 +140,6 @@
             this.pictureBox4.Size = new System.Drawing.Size(172, 310);
             this.pictureBox4.TabIndex = 8;
             this.pictureBox4.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(450, 46);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
             // 
             // label6
             // 
@@ -361,7 +351,6 @@
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -393,7 +382,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
