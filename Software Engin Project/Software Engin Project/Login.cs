feffe7e53ead9e8697cd93b7cc5b9bd5c8dace1a
bomairinge <bomairinge@gmail.com>
2019-11-20 12:12:02 +0000

    Merging

--- Software Engin Project/Software Engin Project/Login.cs
+++ Software Engin Project/Software Engin Project/Login.cs
@@ -26,21 +26,8 @@ namespace Software_Engin_Project
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            // DatabaseConnection.Sample.createDataSet test = ("Select Count(*) from Employee where Username'" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
-            // DataSet employeedata = new Sample.("Select Count(*) from Employee where Username'" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
             int rows = DatabaseConnection.Sample.getSelectCount("Select Count(*) from Employee where Username ='" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
-            /*  DataSet data = DatabaseConnection.createDataSet("Select Count(*) from Employee where Username ='" + textBox1.Text + "' and Password ='" + textBox2.Text + "'");
-              DataTable dt = new DataTable();
-              dt = data.Tables[0];
 
-              //this.Hide(); // Code will need to go    nto an IF statement once database connection created
-              //BedOverview bed = new BedOverview(); // Code will need to go into an IF statement once database connection created
-              //bed.Show(); // Code will need to go into an IF statement once database connection created
-
-
-
-              if (dt.Rows[0][0].ToString() == "1")
-           */
               if(rows==1)
             {
                 this.Hide();
@@ -58,7 +45,13 @@ namespace Software_Engin_Project
             }
             else
             {
-            //Messagebox.Show("No Username or Password found with those credentials");
+                // Initialises message box variables
+                string message = "No Username or Password found with those credentials. Retry?";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                // Displays message box
+                result = Messagebox.Show("");
             } 
         }
 
